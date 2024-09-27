35d45ecd36381daad57b88b335df523de582d929
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2024-09-27 07:11:37 +0000

    test

---
+++ complete/src/main/java/com/example/springboot/Controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.springboot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/test")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
